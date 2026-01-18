deaccf9519dba5fe2ec3a6eeec176bfcbb57e25c
amit <amit101giri@gmail.com>
2026-01-18 19:38:59 +0545

    REFACTOR: use VPC module
    - Replace direct VPC and subnet resource definitions with VPC module
    - Update dependent resources to reference module outputs
    - Improve reusability and consistency across environments

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -24,7 +24,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_security_group" "sonarqube" {
   name        = "${var.environment}-sg"
   description = "Security group for SonarQube EC2"
-  vpc_id      = aws_vpc.this.id
+  vpc_id      = module.vpc.vpc_id
 
   ingress {
     description = "SSH access"
@@ -62,7 +62,7 @@ resource "aws_security_group" "sonarqube" {
 resource "aws_instance" "sonarqube" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.large"
-  subnet_id              = aws_subnet.private[0].id
+  subnet_id              = module.vpc.public_subnets[0].id
   vpc_security_group_ids = [aws_security_group.sonarqube.id]
   iam_instance_profile   = aws_iam_instance_profile.sonarqube.name
   key_name               = var.environment
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -3,125 +3,28 @@ data "aws_vpc" "default" {
   default = true
 }
 
-
 #custom vpc
-resource "aws_vpc" "this" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+module "vpc" {
 
-  tags = merge(
-    var.tags,
-    { Name = "${var.environment}-vpc" }
-  )
+    source = "./modules/vpc"
+    # vpc
+    vpc_name       = var.environment   
+    vpc_cidr_block = var.vpc_cidr
+
+    # az 
+    number_of_az = var.number_of_az
+    vpc_azs      = var.vpc_azs
+
+    # public subnets
+    number_of_public_subnets  = var.number_of_public_subnets     
+    public_subnets_cidr_block = var.public_subnets_cidr_block 
+
+    # private subnets
+    number_of_private_subnets  = var.number_of_private_subnets
+    private_subnets_cidr_block = var.private_subnets_cidr_block
 }
 
-#az
-data "aws_availability_zones" "available" {
-  state = "available"
-}
 
-#public subnets
-resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 2, count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true
 
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.environment}-public-${count.index}"
-      Tier = "public"
-    }
-  )
-}
 
-#private subnets
-resource "aws_subnet" "private" {
-  count             = 2
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 2, count.index + 2)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
 
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.environment}-private-${count.index}"
-      Tier = "private"
-    }
-  )
-}
-
-#internet gateway
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-
-  tags = merge(
-    var.tags,
-    { Name = "${var.environment}-igw" }
-  )
-}
-
-#nat gateway
-resource "aws_eip" "nat" {
-  domain = "vpc"
-
-  tags = merge(
-    var.tags,
-    { Name = "${var.environment}-nat-eip" }
-  )
-}
-
-resource "aws_nat_gateway" "this" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
-
-  tags = merge(
-    var.tags,
-    { Name = "${var.environment}-nat" }
-  )
-}
-
-#route (public)
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
-
-  tags = merge(
-    var.tags,
-    { Name = "${var.environment}-public-rt" }
-  )
-}
-
-resource "aws_route_table_association" "public" {
-  count          = 2
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
-
-#private
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.this.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
-  }
-
-  tags = merge(
-    var.tags,
-    { Name = "${var.environment}-private-rt" }
-  )
-}
-
-resource "aws_route_table_association" "private" {
-  count          = 2
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
-}
