ec859ca0350f30340d8d3e80af650325fffa4a5d
Riwaj Chipalu <69961549+Reewaz-C@users.noreply.github.com>
2026-01-23 20:07:30 +0545

    Update main.tf: Manually added AMI, couldn't  lookup the AMI in eu-north-1 region

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -2,27 +2,27 @@
 # AMI Lookup
 ############################
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+#data "aws_ami" "ubuntu" {
+#  most_recent = true
+#  owners      = ["099720109477"] # Canonical
+#
+#  filter {
+#    name   = "name"
+#    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+#  }
+#
+#  filter {
+#    name   = "virtualization-type"
+#    values = ["hvm"]
+#  }
+#}
 
 ############################
 # EC2 Instance
 ############################
 
 resource "aws_instance" "this" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = "ami-073130f74f5ffb161"
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.sg_id]
